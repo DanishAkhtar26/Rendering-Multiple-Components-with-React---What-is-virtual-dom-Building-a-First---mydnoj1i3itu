eb78e34e374e6c33072f3f87e15d6b1acfc3ffa1
DanishAkhtar26 <102619612+DanishAkhtar26@users.noreply.github.com>
2022-10-17 20:09:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Newton School</h1>
+      <p data-ns-test="project-description">Newton school produces professional web developer</p>
+    </div>
   )
 }
 
